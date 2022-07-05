76aa6bb82f66e8465dab031aa4bb35413e6887d7
Jaya.Choudhary <Administrator@jaya.nectarinfotel.com>
2022-07-05 15:00:33 +0530

    add category vise by plugin

--- wp-content/themes/twentynineteen/functions.php
+++ wp-content/themes/twentynineteen/functions.php
@@ -431,3 +431,10 @@ $args = array(
 register_post_type('news', $args); // Register Post type
 }
 
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+  
+function add_my_post_types_to_query( $query ) {
+    if ( is_home() && $query->is_main_query() )
+        $query->set( 'post_type', array( 'post', 'news' ) );
+    return $query;
+}
